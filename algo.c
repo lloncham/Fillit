b4f62c3607cb863c4e9ae6160ff7089636b76d9f
Lisa LONCHAMP <lloncham@e2r6p10.42.fr>
2018-12-20 11:27:29 +0100

    pas a la norme

--- algo.c
+++ algo.c
@@ -6,26 +6,49 @@
 /*   By: lloncham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 12:23:31 by lloncham          #+#    #+#             */
-/*   Updated: 2018/12/19 17:25:20 by lloncham         ###   ########.fr       */
+/*   Updated: 2018/12/20 11:27:04 by lloncham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		ft_can_place(char **map, t_tris tetris, int *fpos, int size)
+int		ft_can_place(t_tris tetris, int *fpos, int size)
 {
 	int		j;
 
 	j = 0;
 	while (j < 4)
 	{
-		printf("y: %d - x: %d\n", fpos[1] + tetris.content[j][1], fpos[0] + tetris.content[j][0]);
-		if ((fpos[1] + tetris.content[j][1]) && (fpos[0] + tetris.content[j][0] < 0))
+		if ((fpos[1] + tetris.content[j][1]) > 0 && (fpos[0] + tetris.content[j][0] < 0))
 			return (0);
-		if ((fpos[1] + tetris.content[j][1]) > size && (fpos[0] + tetris.content[j][0]) > size)
+		if ((fpos[1] + tetris.content[j][1]) > size - 1 && (fpos[0] + tetris.content[j][0]) > size - 1)
 			return (0);
+		j++;
+	}
+	return (1);
+}
+
+int		check_point(char **map, t_tris tetris, int *fpos, int size)
+{
+	int j;
+
+	j = 0;
+	while (j < 4)
+	{
 		if (map[fpos[1] + tetris.content[j][1]][fpos[0] + tetris.content[j][0]] != '.')
-			return (-1);
+		{
+			if (fpos[0] < size - 1)
+				fpos[0]++;
+			if (fpos[0] == size - 1 && fpos[1] < size - 1)
+			{
+				fpos[1]++;
+				fpos[0] = 0;
+			}
+			if (fpos[0] == size - 1 && fpos[1] == size - 1)
+				return(0);
+			printf("fpos[y] : %d - fpos[x] : %d\n", fpos[1], fpos[0]);
+			check_point(map, tetris, fpos, size);
+		}
 		j++;
 	}
 	return (1);
@@ -38,6 +61,7 @@ int		ft_place(char **map, t_tris tetris, int *fpos, char letter)
 	j = 0;
 	while (j < 4)
 	{
+//		printf("y: %d - x: %d\n", fpos[1] + tetris.content[j][1], fpos[0] + tetris.content[j][0]);
 		map[fpos[1] + tetris.content[j][1]][fpos[0] + tetris.content[j][0]] = letter;
 		j++;
 	}
@@ -56,25 +80,18 @@ int		put_tetris(t_tris tabtetris[], char **map, int size, int nbt)
 	fpos[1] = 0;
 	while (j <= nbt)
 	{
-		if ((ft_can_place(map, *tabtetris, fpos, size)) == 1)
+		if ((ft_can_place(*tabtetris, fpos, size)) == 1)
 		{
-			ft_place(map, *tabtetris, fpos, letter);
-			j++;
-			letter += 1;
-		}
-		if ((ft_can_place(map, *tabtetris, fpos, size)) < 1)
-		{
-			if (fpos[0] <= size)
-				fpos[0]++;
-			if (fpos[0] == size && fpos[1] <= size)
+			if (check_point(map, *tabtetris, fpos, size) == 1)
 			{
-				fpos[0] = 0;
-				fpos[1]++;
+				ft_place(map, *tabtetris, fpos, letter);
+				j++;
+				letter += 1;
+				ft_print_words_tables(map, '\n');
 			}
-			if (fpos[0] == size && fpos[1] == size)
-				return(0);
+			else
+				return (0);
 		}
-		printf("fpos[y] : %d\nfpos[x] : %d\n", fpos[1], fpos[0]);
 		tabtetris++;
 	}
 	return (1);
--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: lloncham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 15:28:36 by lloncham          #+#    #+#             */
-/*   Updated: 2018/12/19 16:31:28 by lloncham         ###   ########.fr       */
+/*   Updated: 2018/12/20 10:56:12 by lloncham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,9 @@ t_tris				new_tetri(char *buff, int len);
 int					read_tetri(t_tris *tabtetris, int fd);
 char				**create_tab(char **map, int size);
 int					min_map(int nb);
-int					ft_can_place(char **map, t_tris tetris, int *fpos, int size);
+int					ft_can_place(t_tris tetris, int *fpos, int size);
 int					ft_place(char **map, t_tris tetris, int *fpos, char letter);
+int					check_point(char **map, t_tris tetris, int *fpos, int size);
 int					put_tetris(t_tris *tabtetris, char **map, int size, int nbt);
 
 #endif
